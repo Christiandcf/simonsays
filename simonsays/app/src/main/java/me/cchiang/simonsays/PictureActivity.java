de998b14042de645a64a652fd79c864dca1e80af
btholmes <btholmes@iastate.edu>
2017-02-05 05:46:48 -0600

    Clean merge

---
+++ simonsays/app/src/main/java/me/cchiang/simonsays/PictureActivity.java
@@ -0,0 +1,272 @@
+//How to Capture Image from Camera and Display in Android ImageView/ Activity
+
+package me.cchiang.simonsays;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.api.ClarifaiResponse;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.input.image.ClarifaiImage;
+import clarifai2.dto.model.ConceptModel;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
+
+import static android.R.id.list;
+import static android.app.Activity.RESULT_OK;
+
+public class PictureActivity extends AppCompatActivity {
+
+    private static final int READ_CONTACTS = 1000;
+    private static final int WRITE_EXTERNAL_STORAGE = 1001;
+    private static final int READ_EXTERNAL_STORAGE = 1002;
+
+    public static boolean CAN_READ_CONTACTS = false;
+    public static boolean CAN_WRITE_EXTERNAL_STORAGE = false;
+    public static boolean CAN_READ_EXTERNAL_STORAGE = false;
+
+
+    private ImageButton cameraButton;
+    private TextView head;
+    private TextView tagText, checkText;
+    private ArrayList<String> tags = new ArrayList<>();
+    private ArrayList<String> checkList = MainActivity.list;
+    private ArrayList<String> leftList = new ArrayList<>();
+
+
+    private final ClarifaiClient clarifaiClient = new ClarifaiBuilder(Credential.CLIENT_ID,
+            Credential.CLIENT_SECRET).buildSync();
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture);
+
+
+        getViews();
+        head.setText(MainActivity.word);
+        handleCameraBtnClick();
+        checkPermissions();
+
+        for (int i = 0 ; i < checkList.size(); i++){
+            String temp = checkList.get(i);
+            leftList.add(temp);
+        }
+    }
+
+
+    private void checkPermissions() {
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_CONTACTS,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            }, WRITE_EXTERNAL_STORAGE);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == READ_CONTACTS) {
+            if(grantResults.length > 0){
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                startActivityForResult(intent, PictureActivity.CAMERA_REQUEST);
+                    CAN_READ_CONTACTS = true;
+                }
+            }
+        }
+        else if(requestCode == WRITE_EXTERNAL_STORAGE){
+            if(grantResults.length > 0){
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    CAN_WRITE_EXTERNAL_STORAGE = true;
+//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                startActivityForResult(intent, PictureActivity.CAMERA_REQUEST);
+                }
+            }
+        }
+        else if (requestCode == READ_EXTERNAL_STORAGE){
+            if(grantResults.length > 0){
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    CAN_READ_EXTERNAL_STORAGE = true;
+//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                startActivityForResult(intent, PictureActivity.CAMERA_REQUEST);
+                }
+            }
+        }
+    }
+
+    /**
+     * Store views for camera and gallery buttons and for the TextView for displaying tags
+     */
+    public void getViews() {
+        cameraButton = (ImageButton) findViewById(R.id.cameraButton);
+        head = (TextView) findViewById(R.id.head);
+        tagText = (TextView) findViewById(R.id.tag_text);
+        checkText = (TextView) findViewById(R.id.check_text);
+    }
+
+    /**
+     * Camera button handler
+     */
+    public void handleCameraBtnClick() {
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                clearFields();
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+        });
+    }
+
+
+    /**
+     * Clears tag values, tag TextView, and preview ImageView
+     */
+    public void clearFields() {
+        tags.clear();
+        tagText.setText("");
+        ((ImageView)findViewById(R.id.picture)).setImageResource(android.R.color.transparent);
+    }
+
+    /**
+     * Prints the first 10 tags for an image
+     */
+    public void printTags() {
+        String results = "TAGS: ";
+        for(int i = 0; i < 10; i++) {
+            results += "\n" + tags.get(i);
+        }
+        tagText.setText(results);
+    }
+
+    // Checks if the words matches
+    public void checkMatch(){
+        String looking = "MATCHED: ";
+
+        // Check for matches and remove match form leftList
+        for(int i = 0; i < 10; i++) {
+            if(checkList.contains(tags.get(i))){
+                looking += "\n" + tags.get(i);
+                leftList.remove(tags.get(i));
+            }
+        }
+
+        looking += "\n\n NEEDS:";
+
+        // Displays whats lefts
+        for(int j = 0; j < leftList.size(); j++){
+            looking += "\n" + leftList.get(j);
+        }
+
+        checkText.setText(looking);
+
+        // If list if empty you win...
+        if(leftList.isEmpty()){
+//            Toast.makeText(this, "I'M PROUD OF YOU", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(PictureActivity.this, Winning.class);
+            startActivity(intent);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        InputStream inStream = null;
+
+        //check if image was collected successfully
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE &&
+                resultCode == RESULT_OK) {
+//                inStream = getContentResolver().openInputStream(data.getData());
+//                Bitmap bitmap = BitmapFactory.decodeStream(inStream);
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            final ImageView preview = (ImageView)findViewById(R.id.picture);
+            preview.setImageBitmap(bitmap);
+
+            new AsyncTask<Bitmap, Void, ClarifaiResponse<List<ClarifaiOutput<Concept>>>>() {
+
+                // Model prediction
+                @Override
+                protected ClarifaiResponse<List<ClarifaiOutput<Concept>>> doInBackground(Bitmap... bitmaps) {
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bitmaps[0].compress(Bitmap.CompressFormat.JPEG, 90, stream);
+                    byte[] byteArray = stream.toByteArray();
+                    final ConceptModel general = clarifaiClient.getDefaultModels().generalModel();
+                    return general.predict()
+                            .withInputs(ClarifaiInput.forImage(ClarifaiImage.of(byteArray)))
+                            .executeSync();
+                }
+
+                // Handling API response and then collecting and printing tags
+                @Override
+                protected void onPostExecute(ClarifaiResponse<List<ClarifaiOutput<Concept>>> response) {
+                    if (!response.isSuccessful()) {
+                        Toast.makeText(getApplicationContext(), "API contact error", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    final List<ClarifaiOutput<Concept>> predictions = response.get();
+                    if (predictions.isEmpty()) {
+                        Toast.makeText(getApplicationContext(), "No results from API", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    final List<Concept> predictedTags = predictions.get(0).data();
+                    for(int i = 0; i < predictedTags.size(); i++) {
+                        tags.add(predictedTags.get(i).name());
+                    }
+                    printTags();
+                    checkMatch();
+                }
+            }.execute(bitmap);
+        }
+    }
+}
+
