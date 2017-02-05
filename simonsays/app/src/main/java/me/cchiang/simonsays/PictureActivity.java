ca783b2f874c9247016ccb040c825d9ab388b121
Christian <christiandcf@gmail.com>
2017-02-05 01:30:54 -0600

    finally got camera to work

--- simonsays/app/src/main/java/me/cchiang/simonsays/PictureActivity.java
+++ simonsays/app/src/main/java/me/cchiang/simonsays/PictureActivity.java
@@ -17,12 +17,15 @@ import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedInputStream;
@@ -39,64 +42,184 @@ import java.io.*;
 import java.util.*;
 import java.text.*;
 
+import clarifai2.api.ClarifaiBuilder;
+import clarifai2.api.ClarifaiClient;
+import clarifai2.api.ClarifaiResponse;
+import clarifai2.dto.input.ClarifaiInput;
+import clarifai2.dto.input.image.ClarifaiImage;
+import clarifai2.dto.model.ConceptModel;
+import clarifai2.dto.model.output.ClarifaiOutput;
+import clarifai2.dto.prediction.Concept;
+
 import static android.app.Activity.RESULT_OK;
 
 public class PictureActivity extends AppCompatActivity {
 
-    private static final int CAMERA_REQUEST = 1888;
-    ImageView mimageView;
+    private static final int READ_CONTACTS = 1000;
+    private static final int WRITE_EXTERNAL_STORAGE = 1001;
+    private static final int READ_EXTERNAL_STORAGE = 1002;
 
-    Uri picUri;
-    String picturePath;
-    Uri selectedImage;
-    Bitmap photo;
-    String ba1;
+    public static boolean CAN_READ_CONTACTS = false;
+    public static boolean CAN_WRITE_EXTERNAL_STORAGE = false;
+    public static boolean CAN_READ_EXTERNAL_STORAGE = false;
 
 
+    private ImageButton cameraButton;
+    private TextView tagText;
+    private ArrayList<String> tags = new ArrayList<>();
+
+    private final ClarifaiClient clarifaiClient = new ClarifaiBuilder(Credential.CLIENT_ID,
+            Credential.CLIENT_SECRET).buildSync();
+
+    private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
+    private static final int GALLERY_IMAGE_ACTIVITY_REQUEST_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_picture);
-
-        mimageView = (ImageView) this.findViewById(R.id.image_from_camera);
-        Button button = (Button) this.findViewById(R.id.take_image_from_camera);
+        getViews();
+        handleCameraBtnClick();
+        checkPermissions();
     }
 
-    public void takeImageFromCamera(View view) {
+    private void checkPermissions() {
 
-        if (getApplicationContext().getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA) ) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
 
-            if(MainActivity.CAN_WRITE_EXTERNAL_STORAGE){
-                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, selectedImage);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            ActivityCompat.requestPermissions(this,new String[]{
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_CONTACTS,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            }, WRITE_EXTERNAL_STORAGE);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == READ_CONTACTS) {
+            if(grantResults.length > 0){
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                startActivityForResult(intent, PictureActivity.CAMERA_REQUEST);
+                    CAN_READ_CONTACTS = true;
+                }
             }
-
-        }else {
-            Toast.makeText(getApplication(), "Camera not supported", Toast.LENGTH_LONG).show();
         }
+        else if(requestCode == WRITE_EXTERNAL_STORAGE){
+            if(grantResults.length > 0){
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    CAN_WRITE_EXTERNAL_STORAGE = true;
+//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                startActivityForResult(intent, PictureActivity.CAMERA_REQUEST);
+                }
+            }
+        }
+        else if (requestCode == READ_EXTERNAL_STORAGE){
+            if(grantResults.length > 0){
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    CAN_READ_EXTERNAL_STORAGE = true;
+//                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                startActivityForResult(intent, PictureActivity.CAMERA_REQUEST);
+                }
+            }
+        }
+    }
 
+    /**
+     * Store views for camera and gallery buttons and for the TextView for displaying tags
+     */
+    public void getViews() {
+        cameraButton = (ImageButton) findViewById(R.id.cameraButton);
+        tagText = (TextView) findViewById(R.id.tag_text);
+    }
+
+    /**
+     * Camera button handler
+     */
+    public void handleCameraBtnClick() {
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                clearFields();
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+            }
+        });
     }
 
 
+    /**
+     * Clears tag values, tag TextView, and preview ImageView
+     */
+    public void clearFields() {
+        tags.clear();
+        tagText.setText("");
+        ((ImageView)findViewById(R.id.picture)).setImageResource(android.R.color.transparent);
+    }
+
+    /**
+     * Prints the first 10 tags for an image
+     */
+    public void printTags() {
+        String results = "First 10 tags: ";
+        for(int i = 0; i < 10; i++) {
+            results += "\n" + tags.get(i);
+        }
+        tagText.setText(results);
+    }
+
+    @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+        InputStream inStream = null;
 
+        //check if image was collected successfully
+        if ((requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE ||
+                requestCode == GALLERY_IMAGE_ACTIVITY_REQUEST_CODE ) &&
+                resultCode == RESULT_OK) {
+//                inStream = getContentResolver().openInputStream(data.getData());
+//                Bitmap bitmap = BitmapFactory.decodeStream(inStream);
+            Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+            final ImageView preview = (ImageView)findViewById(R.id.picture);
+            preview.setImageBitmap(bitmap);
 
-            selectedImage = data.getData();
-//            System.out.println(selectedImage.toString());
+            new AsyncTask<Bitmap, Void, ClarifaiResponse<List<ClarifaiOutput<Concept>>>>() {
 
-            photo = (Bitmap) data.getExtras().get("data");
-            mimageView.setImageBitmap(photo);
+                // Model prediction
+                @Override
+                protected ClarifaiResponse<List<ClarifaiOutput<Concept>>> doInBackground(Bitmap... bitmaps) {
+                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                    bitmaps[0].compress(Bitmap.CompressFormat.JPEG, 90, stream);
+                    byte[] byteArray = stream.toByteArray();
+                    final ConceptModel general = clarifaiClient.getDefaultModels().generalModel();
+                    return general.predict()
+                            .withInputs(ClarifaiInput.forImage(ClarifaiImage.of(byteArray)))
+                            .executeSync();
+                }
 
+                // Handling API response and then collecting and printing tags
+                @Override
+                protected void onPostExecute(ClarifaiResponse<List<ClarifaiOutput<Concept>>> response) {
+                    if (!response.isSuccessful()) {
+                        Toast.makeText(getApplicationContext(), "API contact error", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    final List<ClarifaiOutput<Concept>> predictions = response.get();
+                    if (predictions.isEmpty()) {
+                        Toast.makeText(getApplicationContext(), "No results from API", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
 
+                    final List<Concept> predictedTags = predictions.get(0).data();
+                    for(int i = 0; i < predictedTags.size(); i++) {
+                        tags.add(predictedTags.get(i).name());
+                    }
+                    printTags();
+                }
+            }.execute(bitmap);
         }
-//        upload();
     }
-
-
-
-
-
 }
+
