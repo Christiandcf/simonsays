e8f679a207e0768d77dd66f5d3b624a9a1b04296
Christian <christiandcf@gmail.com>
2017-02-08 22:38:46 -0600

    oops

--- simonsays/app/src/main/java/me/cchiang/simonsays/Credential.java
+++ simonsays/app/src/main/java/me/cchiang/simonsays/Credential.java
@@ -5,6 +5,6 @@ package me.cchiang.simonsays;
  */
 
 public class Credential {
-    static final String CLIENT_ID = "t2MW2db0gR9ywt9aCMk0wqRg2xCLDAmUsNr6UqbH";
-    static final String CLIENT_SECRET = "7PcM-pomF0dr50x5FK2MXi5Xv3n1YijdieLj97A1";
+    static final String CLIENT_ID = "";
+    static final String CLIENT_SECRET = "";
 }
